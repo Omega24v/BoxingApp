b657e1a828cf3bafc3379f06ae5c5e5ad95d281a
Anton <omega24v1993@gmail.com>
2021-11-10 16:05:20 +0200

    add react-bootstrap components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Timer from "./components/timer/Timer";
+import { Container, Row } from "react-bootstrap";
 import './App.sass';
 
 function App() {
   return (
-    <div className="App">
-      <Timer/>
-    </div>
+    <Container>
+      <Row>
+        <Timer/>
+      </Row>
+    </Container>
   );
 }
 
