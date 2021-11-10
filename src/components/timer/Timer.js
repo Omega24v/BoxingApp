c01898d1209c6c110857171ce50625f514153e54
Anton <omega24v1993@gmail.com>
2021-11-10 16:02:46 +0200

    replace elements to react-bootstrap

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {setDefaultValues, startTimer, stopTimer} from "../../store/actions/timerActions";
-
+import { Col, Button } from 'react-bootstrap';
+import cl from './Timer.module.sass';
 
 const Timer = props => {
     return (
-        <div>
-            <button>{props.isEnabled ? 'Stop' : 'Start'}</button>
-            <div>Rounds: {props.rounds}</div>
-            <div>Rounds total time: {props.roundTime}</div>
-            <div>Rounds rest time: {props.restTime}</div>
-            <div>Rounds prepare time: {props.prepareTime}</div>
-        </div>
+        <>
+            <Col lg={6} className={[cl.rounds, cl.dflex].join( )}>
+                <h2 className={cl.rounds__text}>
+                    Rounds
+                </h2> 
+                <span className={cl.rounds__count}>
+                    {props.rounds}
+                </span>
+            </Col>
+            
+            <Col lg={6} className={cl.total}>
+                <h2 className={cl.total__text}>
+                    Rounds total time:
+                </h2>
+                <span className={cl.total__count}>
+                    {props.roundTime}
+                </span>
+            </Col>
+            
+            <Col lg={12}>
+                <div className={cl.rest}>
+                    Rounds rest time: {props.restTime}
+                </div>
+                <div className={cl.prepare}>
+                    Rounds prepare time: {props.prepareTime}
+                </div>
+            </Col>
+            <Button variant="success">{props.isEnabled ? 'Stop' : 'Start'}</Button>
+        </>
     );
 };
 
