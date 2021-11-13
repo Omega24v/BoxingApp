ad44458f5db6a2a0a7b4dc55658e28187351f55b
Anton <omega24v1993@gmail.com>
2021-11-13 17:39:45 +0200

    remove inline styles for timer-list

--- src/components/timersList/TimersList.js
+++ src/components/timersList/TimersList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { CloseButton, Col, Row } from 'react-bootstrap';
 import {connect} from "react-redux";
 import {addTimer, setTimer, startTimer} from "../../store/actions/timerActions";
+import millisToMinutesAndSeconds from '../../utils/timeConverter';
+import './TimersList.sass'
 
 const TimersList = props => {
     return (
@@ -9,17 +11,16 @@ const TimersList = props => {
         <div className="d-flex flex-wrap gap-3">
             {props.timers.map((timer, index) =>
                 <div 
-                    className="timer-list flex-grow-1"
-                    style={{border: '1px solid #777', padding: '7px 10px', cursor: 'pointer'}}
+                    className="timer-list flex-grow-1 p-2"
                     onClick={() => props.setTimer(timer)}
                     key={timer.id}>
                     <Row>
                         <Col lg={9}>
                             <div className={`timer-list__item mb-2 ${timer.id === props.currTimer.id ? 'text-danger' : ''}`}>{timer.name}</div>
                             <div className="timer-list__item text-warning">Rounds: {timer.rounds}</div>
-                            <div className="timer-list__item text-warning">Rounds Time: {timer.roundTime}</div>
-                            <div className="timer-list__item text-warning">Rest Time: {timer.restTime}</div>
-                            <div className="timer-list__item text-warning">Prepare Time: {timer.prepareTime}</div>
+                            <div className="timer-list__item text-warning">Rounds Time: { millisToMinutesAndSeconds((timer.roundTime)) }</div>
+                            <div className="timer-list__item text-warning">Rest Time: { millisToMinutesAndSeconds((timer.restTime)) }</div>
+                            <div className="timer-list__item text-warning">Prepare Time: { millisToMinutesAndSeconds((timer.prepareTime)) }</div>
                         </Col>
                         <Col lg={3} className="d-flex justify-content-end">
                             <CloseButton variant="white" />
