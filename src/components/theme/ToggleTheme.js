920e60eb6c35f19aa127a327fbde48d094ecb0f7
Anton <omega24v1993@gmail.com>
2021-11-16 11:12:27 +0200

    add switchChecked func & change switch btn structure

--- src/components/theme/ToggleTheme.js
+++ src/components/theme/ToggleTheme.js
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {Form} from "react-bootstrap";
 import {loadData, setData} from "../../utils/localStorage/localStorage";
 import {THEME_DARK, THEME_LIGHT} from "../../constatns/theme";
+import './ToggleTheme.sass'
 
 const ToggleTheme = () => {
-
     const [currTheme, setCurrTheme] = useState(THEME_DARK);
 
     const setThemeToBody = theme => {
@@ -12,28 +12,36 @@ const ToggleTheme = () => {
         body.setAttribute("data-theme", theme);
     }
 
+    const switchChecked = (currTheme) => {
+        console.log(currTheme)
+        const switchBtn = document.querySelector("#custom-switch");
+        currTheme === "dark" ? switchBtn.checked = true : switchBtn.checked = false
+    }
+
     const toggleTheme = () => {
         const theme = currTheme === THEME_DARK ? THEME_LIGHT : THEME_DARK;
         setCurrTheme(theme);
         setThemeToBody(theme);
+        
         setData(theme, 'theme');
     }
+    
+
 
     useEffect(() => {
         const currTheme = loadData('theme');
         setCurrTheme(currTheme || THEME_DARK);
-        setThemeToBody(currTheme|| THEME_DARK);
+        setThemeToBody(currTheme || THEME_DARK);
+        switchChecked(currTheme)
     }, []);
 
     return (
-        <Form>
-            <Form.Check
-                onChange={() => toggleTheme()}
-                type="switch"
-                id="custom-switch"
-                label={currTheme}
-            />
-        </Form>
+        <div className="theme-switch">
+            <input type="checkbox" id="custom-switch" name="theme-switch" class="theme-switch__input" onChange={() => toggleTheme()}/>
+            <label for="custom-switch" class="theme-switch__label" label={currTheme}>
+                <span></span>
+            </label>
+        </div>
     );
 };
 
