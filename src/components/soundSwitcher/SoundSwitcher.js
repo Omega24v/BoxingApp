cf721d411e03a505114136ddd10c4735aee71d44
Anton <omega24v1993@gmail.com>
2021-11-17 16:35:07 +0200

    change sound switcher

--- src/components/soundSwitcher/SoundSwitcher.js
+++ src/components/soundSwitcher/SoundSwitcher.js
@@ -3,27 +3,33 @@ import { Form } from 'react-bootstrap';
 import {connect} from "react-redux";
 import {toggleSound} from "../../store/actions/timerActions";
 import {loadData} from "../../utils/localStorage/localStorage";
+import './SoundSwitcher.sass';
 
 const SoundSwitcher = props => {
 
     useEffect(() => {
         const isSound = loadData('isSound');
-        if (isSound != props.isSound) {
+        if (isSound !== props.isSound) {
             props.toggleSound();
         }
     }, []);
 
     return (
-        <div>
-            <Form>
-                <Form.Check
-                    type="switch"
-                    checked={props.isSound}
+        <>
+            <div className="sound-switch me-4">
+                <input 
+                    type="checkbox" 
+                    id="sd-switch" 
+                    name="sound-switch" 
+                    className="sound-switch__input" 
                     onChange={() => props.toggleSound()}
-                    label={props.isSound ? 'Sound on' : 'Sound off' }
-                />
-            </Form>
-        </div>
+                    checked={props.isSound}
+                    />
+                <label htmlFor="sd-switch" className="sound-switch__label" label={props.isSound ? 'Sound on' : 'Sound off' }>
+                    <span></span>
+                </label>
+            </div>
+        </>
     );
 };
 
