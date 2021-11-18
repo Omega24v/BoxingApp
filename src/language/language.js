b8e704ace17f20f3bd71464835fe45eecc619261
Anton <omega24v1993@gmail.com>
2021-11-18 15:26:30 +0200

    add language object

---
+++ src/language/language.js
@@ -0,0 +1,48 @@
+export const language = {
+  ua: {
+    rest: "відпочинок",
+    total_time:"весь час",
+    of: "з",
+    save_settings: "зберегти налаштування",
+    save_as_new_timer: "зберегти новий таймер",
+    default: "за замовчуванням",
+    warning: "на готові",
+    timer_name: "назва таймеру",
+    start: "початок",
+    prepare: "приготуватись",
+    round: "раунд",
+    rounds: "раунди",
+    edit: "редагувати",
+    create_timer: "створити таймер",
+    phase: "фаза",
+    prepare_time: "час на підготовку",
+    round_time: "час раунду",
+    close: "закрити"
+  },
+  "us": {
+    close: "close",
+    current_round: "current round",
+    default: "default",
+    edit: "edit",
+    full_time: "full time",
+    of: "of",
+    phase: "phase",
+    prepare_time: "prepare time",
+    prepare: "prepare",
+    rest_time: "rest time",
+    rest: "rest",
+    round_time: "round time",
+    round: "round",
+    rounds: "rounds",
+    s: "s",
+    save_as_new_timer: "save as new timer",
+    save_settings: "save settings",
+    start: "start",
+    timer_name: "timer name",
+    total_time: "total time",
+    warning_time: "warning time",
+    warning: "warning"
+  }
+}
+
+
