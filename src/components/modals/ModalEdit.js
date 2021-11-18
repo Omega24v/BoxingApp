f381574fabc3b88614e73f0fa6c0ed067636743c
Anton <omega24v1993@gmail.com>
2021-11-18 20:14:01 +0200

    add CloseButton bootstrap component

--- src/components/modals/ModalEdit.js
+++ src/components/modals/ModalEdit.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Button, CloseButton } from 'react-bootstrap'
+import IconClose from '../../icons/IconClose';
 import TimerEdit from '../timerEdit/TimerEdit';
+import './ModalEdit.sass';
 
 const Modals = props => {
   const { timerName, ...rest } = props;
   return (
     <Modal className="edit-modal" {...rest} size="md" aria-labelledby="modal-title" centered>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>
           Edit {timerName}
         </Modal.Title>
+        <CloseButton aria-label="Close"><IconClose/></CloseButton>
       </Modal.Header>
       <Modal.Body>
         <TimerEdit/>
