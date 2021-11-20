60ebd6eca427b2d793966db1c97bcc2c0aa286e3
Anton <omega24v1993@gmail.com>
2021-11-20 22:35:16 +0200

    add intlProvider to App

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import {Container, Row, Col} from "react-bootstrap";
 import ToggleTheme from "./components/theme/ToggleTheme";
 import './App.sass';
 import SoundSwitcher from "./components/soundSwitcher/SoundSwitcher";
+import { IntlProvider } from "react-intl";
+import { LOCALES } from './translation/locales'
+import { messages } from './translation/messages'
 
 function App() {
+  console.dir(IntlProvider)
+  const locale = LOCALES.ENGLISH
+
   return (
-    <div className="App">
-      <Container>
-        <Row className="mt-2">
-          <Col lg={12} className="d-flex justify-content-between">
-            <ToggleTheme/>
-            <SoundSwitcher/>
-          </Col>
-        </Row>
-        <div className="justify-content-center">
-          <Col lg={12} className="timer my-3 p-3">
-            <Timer/>
-          </Col>
-        </div>
-      </Container>
-    </div>
+    <IntlProvider messages={messages[locale]} locale={locale} defaultLocale={LOCALES.ENGLISH}>
+      <div className="App">
+        <Container>
+          <Row className="mt-2">
+            <Col lg={12} className="d-flex justify-content-between">
+              <ToggleTheme/>
+              <SoundSwitcher/>
+            </Col>
+          </Row>
+          <div className="justify-content-center">
+            <Col lg={12} className="timer my-3 p-3">
+              <Timer/>
+            </Col>
+          </div>
+        </Container>
+      </div>
+    </IntlProvider>
   );
 }
 
