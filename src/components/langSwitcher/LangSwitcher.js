de9b272b2327ee0067754ff63dedc1d4f185b21e
taras <slideua@gmail.com>
2021-11-23 12:44:16 +0200

    BA-15 small fixes

--- src/components/langSwitcher/LangSwitcher.js
+++ src/components/langSwitcher/LangSwitcher.js
@@ -5,32 +5,35 @@ import FlagImg from '../icons/FlagImg';
 import './LangSwitcher.sass';
 
 const LangSwitcher = (props) => {
-  const { currentLocale, setCurrentLocale} = props;
 
+  const { currentLocale, setCurrentLocale} = props;
   const handleChange = (code) => {
-    props.setCurrentLocale(code)
+    setCurrentLocale(code);
     localStorage.setItem("lang", code);
   }
 
   return (
-    <>
-      <Form>
-        <Dropdown className="lang-switcher" value={currentLocale}>
-
-          <Dropdown.Toggle variant="light" className="lang-switcher__toggler p-2 d-flex align-items-center">
-            <div className="d-flex align-items-center"><FlagImg code={currentLocale}/><span className="switcher-title">{LOCALES[currentLocale].name}</span></div>
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            {Object.keys(LOCALES).map((lang) => (
-              <Dropdown.Item onClick={() => {handleChange(LOCALES[lang].code)}}>
-                <div className="d-flex align-items-center"><FlagImg code={lang}/><span class="switcher-title">{LOCALES[lang].name}</span></div>
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-      </Form>
-    </>
+    <Form>
+      <Dropdown className="lang-switcher" value={currentLocale}>
+        <Dropdown.Toggle variant="light" className="lang-switcher__toggler p-2 d-flex align-items-center">
+          <div className="d-flex align-items-center">
+            <FlagImg code={currentLocale}/>
+            <span className="switcher-title">
+            {LOCALES[currentLocale].name}</span>
+          </div>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {Object.keys(LOCALES).map((lang) => (
+            <Dropdown.Item onClick={() => {handleChange(LOCALES[lang].code)}}>
+              <div className="d-flex align-items-center">
+                <FlagImg code={lang}/>
+                <span className="switcher-title">{LOCALES[lang].name}</span>
+              </div>
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Form>
   )
 }
 
