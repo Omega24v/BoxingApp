566503fb362ac0046c38f6b50fbf5c13f62c86a2
taras <slideua@gmail.com>
2021-12-04 14:30:27 +0200

    Fix text, patch robots.txt

--- src/translation/messages.js
+++ src/translation/messages.js
@@ -31,8 +31,11 @@ export const messages = {
     wantDelete: "Do you really want to delete",
     warning: "Warning",
     editAdd: "Edit/Add",
-    popoverHeaderText: "What is inner alerts?",
-    popoverBodyText: "To set an internal alert, simply list the number of seconds to turn on each subsequent alert. For example, if you enter the number 10, the notification will be every 10 seconds. If you enter 10, 20 alerts will be first after ten seconds and then after 20 seconds and so on in a circle throughout the round.",
+    popoverHeaderText: "How to configure inner alerts?",
+    popoverBodyText: `To set an inner alert, simply enter the number of seconds to turn on each subsequent alert.{br}{br}
+        For example, if you enter the number 10, the notification will be every 10 seconds.{br}{br}
+        If you enter 10, 20 first alert will sound after ten seconds and then after 20 seconds and so on in a circle throughout the round.{br}{br}
+        You can enter any number of alerts.`,
   },
   [LOCALES.UK.code]: {
     delete: "Видалити",
@@ -40,7 +43,7 @@ export const messages = {
     totalTime: "Увесь час",
     of: "з",
     saveSettings: "Зберегти налаштування",
-    saveAsNewTimer: "Зберегти новий таймер",
+    saveAsNewTimer: "Зберегти як новий таймер",
     default: "За замовчуванням",
     warning: "На готові",
     warningTime: "Попередження",
@@ -63,8 +66,11 @@ export const messages = {
     roundTime: "Час раунду",
     close: "Закрити",
     editAdd: "Редагувати/додати",
-    popoverHeaderText: "Що таке циклічні сповіщення?",
-    popoverBodyText: "Для того щоб задати внутрішнє оповіщення просто перерахуйте через кому через яку кількість секунд вмикати кожне наступне оповіщення. Наприклад якщо ввести число 10 то оповіщення буде кожні 10 секунд. Якщо ввести 10, 20 оповіщення буде спочатку після десяти  секунд, а потім після 20 секунд і так по колу на протязі раунд.",
+    popoverHeaderText: "Як налаштувати внутрішні сповіщення?",
+    popoverBodyText: `Для того щоб задати внутрішнє оповіщення просто перерахуйте через кому через яку кількість секунд вмикати кожне наступне оповіщення.{br}{br}
+     Наприклад якщо ввести число 10 то оповіщення буде кожні 10 секунд.{br}{br}
+      Якщо ввести 10, 20 оповіщення буде спочатку після десяти секунд, а потім після 20 секунд і так по колу на протязі раунду.{br}{br}
+      Ви можете ввести будь-яку кількість сповіщень.`,
   },
   [LOCALES.ES.code]: {
     close: "Cerrar",
@@ -95,7 +101,10 @@ export const messages = {
     wantDelete: "Realmente quieres borrar",
     warning: "Advertencia",
     editAdd: "Editar/Añadir",
-    popoverHeaderText: "Qué son las alertas internas?",
-    popoverBodyText: "Para configurar una alerta interna, simplemente enumere la cantidad de segundos para activar cada alerta posterior. Por ejemplo, si ingresa el número 10, la notificación será cada 10 segundos. Si ingresa 10, 20 alertas serán primero después de diez segundos y luego después de 20 segundos y así sucesivamente en un círculo a lo largo de la ronda.",
+    popoverHeaderText: "¿Cómo configurar las alertas internas?",
+    popoverBodyText: `Para configurar una alerta interna, simplemente ingrese la cantidad de segundos para activar cada alerta posterior.{br}{br}
+        Por ejemplo, si ingresa el número 10, la notificación será cada 10 segundos.{br}{br}
+        Si ingresa 10, 20 la primera alerta 20 sonará después de diez segundos y luego después de 20 segundos y así sucesivamente en un círculo a lo largo de la ronda.{br}{br}
+        Puede ingresar cualquier cantidad de notificaciones.`,
   },
 }
