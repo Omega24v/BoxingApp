077959fdb624648c93c8b33acc331ef3e1cb7622
omega24v <omega24v1993@gmail.com>
2021-12-05 15:06:23 +0200

    fix lg-switcher img attr

--- src/components/icons/FlagImg.js
+++ src/components/icons/FlagImg.js
@@ -19,7 +19,7 @@ import { Image } from 'react-bootstrap';
   }
 
   return (
-    <Image src={getSvg(code)} width="1.7vw" height="auto" className="switcher-flag" alt="lang-flag" />
+    <Image src={getSvg(code)} width="22" height="14" className="switcher-flag" alt="lang-flag" />
   )
 }
 
