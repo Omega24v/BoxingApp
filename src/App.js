b70ab7e188a19ca8a4f03c4a161d97fb5a4f3ed7
omega24v <omega24v1993@gmail.com>
2021-12-08 11:42:28 +0200

    transfer lang. component to Timer.js & add new style

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
-import React, { useState } from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import { IntlProvider } from "react-intl";
-import Timer from "./components/timer/Timer";
-import ToggleTheme from "./components/theme/ToggleTheme";
-import SoundSwitcher from "./components/soundSwitcher/SoundSwitcher";
-import { LOCALES } from './translation/locales';
-import { messages } from './translation/messages';
-import LangSwitcher from "./components/langSwitcher/LangSwitcher";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Timer from './components/timer/Timer';
+import Copyright from './components/copyright/Сopyright';
 import './App.sass';
-import Copyright from "./components/copyright/Сopyright";
 
 function App() {
-
-  const [currentLocale, setCurrentLocale] =  useState(getInitialLocal());
-  const defaultLoc = LOCALES.EN.code;
-
-  function getInitialLocal() {
-    const savedLocale = localStorage.getItem("lang");
-    return savedLocale || LOCALES.EN.code;
-  }
-
   return (
-    <IntlProvider messages={messages[currentLocale]} locale={currentLocale} defaultLocale={defaultLoc}>
-      <div className="App">
-        <Container>
-          <Row className="mt-2">
-            <Col lg={12} className="d-flex justify-content-between align-items-center">
-              <ToggleTheme/>
-              <LangSwitcher currentLocale={currentLocale} setCurrentLocale={setCurrentLocale} defaultLoc={defaultLoc} />
-              <SoundSwitcher/>
-            </Col>
-          </Row>
-          <div className="justify-content-center">
-            <Col lg={12} className="timer my-2 p-3">
-              <Timer/>
-            </Col>
-          </div>
-          <Copyright/>
-        </Container>
-      </div>
-    </IntlProvider>
+    <div className="App">
+      <Container>
+        <Row>
+          <Col
+            lg={12}
+            className="d-flex justify-content-between align-items-center"
+          ></Col>
+        </Row>
+        <div className="justify-content-center">
+          <Col lg={12} className="timer my-2 p-3">
+            <Timer />
+          </Col>
+        </div>
+        <Copyright />
+      </Container>
+    </div>
   );
 }
 
-export default App
+export default App;
